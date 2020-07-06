dd0b8a4a836908090e69d660eddadfe99f079e2d
JosephCollins Udeani <JosephCollins.Udeani@aa.com>
2020-07-05 20:10:42 -0500

    Add project files.

---
+++ TheCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace TheCodeCamp.Controllers
+{
+    public class CampsController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return Ok(new { Name = "Shawn", Occupation = "Teacher" });
+        }
+    }
+}
